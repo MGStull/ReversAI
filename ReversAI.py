77caa5cdb2e2e4aa23d3167ee6ad20720355996c
mstull <mstull@westmont.edu>
2025-06-11 12:46:15 -0700

    Setting up project files

---
+++ ReversAI.py
@@ -0,0 +1,10 @@
+import Reversi.py
+import TensorFlow as TF
+import MLP.py
+
+#Goals
+#   1. MLP combined with Monte Carlos Tree Search with adjustable search depth
+#   2. Create Self Play option for training if neccessary
+#   3. Train on Online Data sets and convert them to useful format
+#   4. Download the Trained Model and implement it on the website
+
